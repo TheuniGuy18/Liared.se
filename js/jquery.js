8a7429ac57d715c4fc64d0504c54517856427f8c
Arvid <arvid.sandstedt@gmail.com>
2020-04-24 14:40:57 +0200

    lite blandat

---
+++ js/jquery.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+      
+    $(".test").on("load",function() {
+    
+      $(this).animate({
+        width: "600px",
+        height: "300px", 
+         }, 1500 );
+      });
+});
+
+
